42ec91018687cc90fc49b375eeb0039e9d81eb95
NaveedAhmedRazvi <35389812+NaveedAhmedRazvi@users.noreply.github.com>
2020-06-08 11:56:15 +0530

    changes

--- examples/two-tier/variables.tf
+++ examples/two-tier/variables.tf
@@ -1,28 +1,19 @@
-variable "public_key_path" {
-  description = <<DESCRIPTION
-Path to the SSH public key to be used for authentication.
-Ensure this keypair is added to your local SSH agent so provisioners can
-connect.
-
-Example: ~/.ssh/terraform.pub
-DESCRIPTION
-}
-
-variable "key_name" {
-  description = "Desired name of AWS key pair"
-}
-
 variable "aws_region" {
-  description = "AWS region to launch servers."
-  default     = "us-west-2"
+  description = "AWS region"
+  default = "us-west-1"
 }
 
-# Ubuntu Precise 12.04 LTS (x64)
-variable "aws_amis" {
-  default = {
-    eu-west-1 = "ami-674cbc1e"
-    us-east-1 = "ami-1d4e7a66"
-    us-west-1 = "ami-969ab1f6"
-    us-west-2 = "ami-8803e0f0"
-  }
+variable "ami_id" {
+  description = "ID of the AMI to provision. Default is Ubuntu 14.04 Base Image"
+  default = "ami-2e1ef954"
+}
+
+variable "instance_type" {
+  description = "type of EC2 instance to provision."
+  default = "t2.micro"
+}
+
+variable "name" {
+  description = "name to pass to Name tag"
+  default = "Provisioned by Terraform"
 }
